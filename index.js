58e9cbe5e036afabc6e7f3aa272610ae1a2e65af
Adam Weaver <adamweaver90@gmail.com>
2022-09-20 15:44:57 -0400

    commit tree c41e745822f8dc5e2d70633f995b46df0a44ec4b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+main.remove();
+
+
+//1) has a 'newHeader' variable that points to an <h1> node
+document.body.append(h1);
+let newHeader = document.querySelector('h1');
+
+//2) the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+newHeader.h1.id = victory;
+
+//3) the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text "YOUR-NAME is the champion" (with your name in place of YOUR-NAME) inside it
+document.querySelector('h1').textContent = 'Adam is the champion'
+
+
+const renderHeader = document.querySelector('h1').textContent = 'Adam is the champion';
+    
